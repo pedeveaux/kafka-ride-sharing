b111e44e4fc44db0f12d0ee776a4f6695a1d1d0c
Paul Deveaux <paul@pauldeveaux.com>
2025-05-26 15:20:48 +0000

    Make db layer idempotent upgrade to postgres 17

--- rides_db/insert.go
+++ rides_db/insert.go
@@ -16,6 +16,7 @@ func InsertRideEvent(ctx context.Context, e events.RideEvent) error {
         INSERT INTO ride_events 
         (id, trip_id, event_type, event_state, event_time, driver_id, passenger_id, payload)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        ON CONFLICT (trip_id, event_type) DO NOTHING
     `, e.ID, e.TripID, e.Type, e.State, e.Timestamp, e.DriverID, e.PassengerID, payloadBytes)
 
     return err
