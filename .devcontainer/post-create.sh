fa0697cd24a019320faae0a54c1014edb854a612
Paul Deveaux <paul@pauldeveaux.com>
2025-05-27 00:02:26 +0000

    clean up shell script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -20,9 +20,3 @@ sed -i 's/ZSH_THEME=.*/ZSH_THEME="agnoster"/' ~/.zshrc
 echo 'export GOPATH=$HOME' >> ~/.zshrc
 echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.zshrc
 echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.zshrc
-
-# go install github.com/brianvoe/gofakeit/v6@latest
-# go install github.com/confluentinc/confluent-kafka-go/kafka@latest
-
-# Reload .zshrc file - This isn't needed as this is only run when the container is created
-# source ~/.zshrc
